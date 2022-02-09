e4cf01066c62f0850219435eb2ef352e480747b9
tebin <ohjiwoo123@naver.com>
2022-02-09 12:44:25 +0900

    초기 커밋.

---
+++ CSharpPaint/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace CSharpPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.도형그리기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.직선ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.원ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.네모ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.채우기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.굵게ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장및불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.불러오기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.도형그리기ToolStripMenuItem,
+            this.저장및불러오기ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.MenuActivate += new System.EventHandler(this.menuStrip1_MenuActivate);
+            // 
+            // 도형그리기ToolStripMenuItem
+            // 
+            this.도형그리기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.직선ToolStripMenuItem,
+            this.원ToolStripMenuItem,
+            this.네모ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.채우기ToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.굵게ToolStripMenuItem});
+            this.도형그리기ToolStripMenuItem.Name = "도형그리기ToolStripMenuItem";
+            this.도형그리기ToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.도형그리기ToolStripMenuItem.Text = "도형그리기";
+            // 
+            // 직선ToolStripMenuItem
+            // 
+            this.직선ToolStripMenuItem.Name = "직선ToolStripMenuItem";
+            this.직선ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.직선ToolStripMenuItem.Text = "직선";
+            this.직선ToolStripMenuItem.Click += new System.EventHandler(this.직선ToolStripMenuItem_Click);
+            // 
+            // 원ToolStripMenuItem
+            // 
+            this.원ToolStripMenuItem.Name = "원ToolStripMenuItem";
+            this.원ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.원ToolStripMenuItem.Text = "원";
+            this.원ToolStripMenuItem.Click += new System.EventHandler(this.원ToolStripMenuItem_Click);
+            // 
+            // 네모ToolStripMenuItem
+            // 
+            this.네모ToolStripMenuItem.Name = "네모ToolStripMenuItem";
+            this.네모ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.네모ToolStripMenuItem.Text = "네모";
+            this.네모ToolStripMenuItem.Click += new System.EventHandler(this.네모ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 채우기ToolStripMenuItem
+            // 
+            this.채우기ToolStripMenuItem.Name = "채우기ToolStripMenuItem";
+            this.채우기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.채우기ToolStripMenuItem.Text = "채우기";
+            this.채우기ToolStripMenuItem.Click += new System.EventHandler(this.채우기ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // 굵게ToolStripMenuItem
+            // 
+            this.굵게ToolStripMenuItem.Name = "굵게ToolStripMenuItem";
+            this.굵게ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.굵게ToolStripMenuItem.Text = "굵게";
+            this.굵게ToolStripMenuItem.Click += new System.EventHandler(this.굵게ToolStripMenuItem_Click);
+            // 
+            // 저장및불러오기ToolStripMenuItem
+            // 
+            this.저장및불러오기ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.저장ToolStripMenuItem,
+            this.불러오기ToolStripMenuItem});
+            this.저장및불러오기ToolStripMenuItem.Name = "저장및불러오기ToolStripMenuItem";
+            this.저장및불러오기ToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.저장및불러오기ToolStripMenuItem.Text = "저장 및 불러오기";
+            // 
+            // 저장ToolStripMenuItem
+            // 
+            this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+            this.저장ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.저장ToolStripMenuItem.Text = "저장";
+            this.저장ToolStripMenuItem.Click += new System.EventHandler(this.저장ToolStripMenuItem_Click);
+            // 
+            // 불러오기ToolStripMenuItem
+            // 
+            this.불러오기ToolStripMenuItem.Name = "불러오기ToolStripMenuItem";
+            this.불러오기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.불러오기ToolStripMenuItem.Text = "불러오기";
+            this.불러오기ToolStripMenuItem.Click += new System.EventHandler(this.불러오기ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 도형그리기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 직선ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 원ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 네모ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 채우기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem 굵게ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장및불러오기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 불러오기ToolStripMenuItem;
+    }
+}
+
---
+++ CSharpPaint/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpPaint
+{
+    // enum enumShape { Line, Circle, Rectangle };
+    public partial class Form1 : Form
+    {
+        public bool m_bDrag;
+        public bool m_bFill;
+        public bool m_bBold;
+        public int x, y, x2, y2;
+        public static Point startPoint;
+        public static Point endPoint;
+        public int m_nShape;
+        Shape MyShape = new Shape(startPoint, endPoint, false, false, 0);
+        List<Shape> ar = new List<Shape>();
+        public Form1()
+        {
+            InitializeComponent();
+            startPoint.X = 0;
+            startPoint.Y = 0;
+            endPoint.X = 0;
+            endPoint.Y = 0;
+            m_nShape = 0;
+            m_bFill = false;
+            m_bDrag = false;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush brush = new SolidBrush(Color.Empty);
+            Pen pen = new Pen(Color.Black);
+
+            for (int i = 0; i < ar.Count; i++)
+            {
+                float w = Math.Abs(ar[i].startPoint.X - ar[i].endPoint.X);
+                float h = Math.Abs(ar[i].startPoint.Y - ar[i].endPoint.Y);
+
+                if (ar[i].m_bFill)
+                {
+                    brush = new SolidBrush(Color.Red);
+                }
+
+                if (ar[i].m_bBold)
+                {
+                    pen.Width = 8.0F;
+                }
+
+                switch (ar[i].m_nShape)
+                {
+                    case 0:
+                        e.Graphics.DrawLine(pen, ar[i].startPoint, ar[i].endPoint);
+                        break;
+                    case 1:
+                        if (ar[i].endPoint.X > ar[i].startPoint.X)
+                        {
+                            if (ar[i].endPoint.Y > ar[i].startPoint.Y)
+                            {
+                                e.Graphics.DrawEllipse(pen, ar[i].startPoint.X, ar[i].startPoint.Y, w, h);
+                                e.Graphics.FillEllipse(brush, ar[i].startPoint.X, ar[i].startPoint.Y, w, h);
+                            }
+                            else
+                            {
+                                e.Graphics.DrawEllipse(pen, ar[i].startPoint.X, ar[i].endPoint.Y, w, h);
+                                e.Graphics.FillEllipse(brush, ar[i].startPoint.X, ar[i].endPoint.Y, w, h);
+                            }
+                        }
+                        else
+                        {
+                            if (ar[i].endPoint.Y > ar[i].startPoint.Y)
+                            {
+                                e.Graphics.DrawEllipse(pen, ar[i].endPoint.X, ar[i].startPoint.Y, w, h);
+                                e.Graphics.FillEllipse(brush, ar[i].endPoint.X, ar[i].startPoint.Y, w, h);
+                            }
+                            else
+                            {
+                                e.Graphics.DrawEllipse(pen, ar[i].endPoint.X, ar[i].endPoint.Y, w, h);
+                                e.Graphics.FillEllipse(brush, ar[i].endPoint.X, ar[i].endPoint.Y, w, h);
+                            }
+                        }
+                        break;
+                    case 2:
+                        if (ar[i].endPoint.X > ar[i].startPoint.X)
+                        {
+                            if (ar[i].endPoint.Y > ar[i].startPoint.Y)
+                            {
+                                e.Graphics.DrawRectangle(pen, ar[i].startPoint.X, ar[i].startPoint.Y, w, h);
+                                e.Graphics.FillRectangle(brush, ar[i].startPoint.X, ar[i].startPoint.Y, w, h);
+                            }
+                            else
+                            {
+                                e.Graphics.DrawRectangle(pen, ar[i].startPoint.X, ar[i].startPoint.Y, w, h);
+                                e.Graphics.FillRectangle(brush, ar[i].startPoint.X, ar[i].endPoint.Y, w, h);
+                            }
+                        }
+                        else
+                        {
+                            if (ar[i].endPoint.Y > ar[i].startPoint.Y)
+                            {
+                                e.Graphics.DrawRectangle(pen, ar[i].endPoint.X, ar[i].startPoint.Y, w, h);
+                                e.Graphics.FillRectangle(brush, ar[i].endPoint.X, ar[i].startPoint.Y, w, h);
+                            }
+                            else
+                            {
+                                e.Graphics.DrawRectangle(pen, ar[i].endPoint.X, ar[i].endPoint.Y, w, h);
+                                e.Graphics.FillRectangle(brush, ar[i].endPoint.X, ar[i].endPoint.Y, w, h);
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Shape MyShape = new Shape(startPoint, endPoint, false, false, 0);
+            if (e.Button == MouseButtons.Left)
+            {
+                m_bDrag = true;
+                startPoint = new Point(e.X, e.Y);
+                MyShape.startPoint = startPoint;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (m_bDrag)
+                {
+                    endPoint = new Point(e.X, e.Y);
+                    MyShape.endPoint = endPoint;
+                    Invalidate();
+                }
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (m_bDrag)
+                {
+                    endPoint = new Point(e.X, e.Y);
+                    MyShape.endPoint = endPoint;
+                    MyShape.startPoint = startPoint;
+                    MyShape.m_bFill = m_bFill;
+                    MyShape.m_bBold = m_bBold;
+                    MyShape.m_nShape = m_nShape;
+                    //Shape Myshape = new Shape(startPoint, endPoint, m_bFill, m_bBold, m_nShape);
+                    ar.Add(MyShape);
+                    Invalidate();
+
+                    // Point 값들 초기화 
+                    startPoint.X = 0;
+                    startPoint.Y = 0;
+                    endPoint.X = 0;
+                    endPoint.Y = 0;
+
+                    // 객체에 추가한 후에 m_bFill, m_bBold, m_nShape 는 상태를 유지하고 x,y값은 초기화해서 shape class를 다시 할당 
+                    MyShape = new Shape(startPoint, endPoint, m_bFill, m_bBold, m_nShape);
+                    // Invalidate();
+                    m_bDrag = false;
+                }
+            }
+        }
+
+        private void menuStrip1_MenuActivate(object sender, EventArgs e)
+        {
+            switch (m_nShape)
+            {
+                case 0:
+                    직선ToolStripMenuItem.Checked = true;
+                    원ToolStripMenuItem.Checked = false;
+                    네모ToolStripMenuItem.Checked = false;
+                    break;
+                case 1:
+                    원ToolStripMenuItem.Checked = true;
+                    직선ToolStripMenuItem.Checked = false;
+                    네모ToolStripMenuItem.Checked = false;
+                    break;
+                case 2:
+                    네모ToolStripMenuItem.Checked = true;
+                    직선ToolStripMenuItem.Checked = false;
+                    원ToolStripMenuItem.Checked = false;
+                    break;
+            }
+
+            if (m_bBold)
+            {
+                굵게ToolStripMenuItem.Checked = true;
+            }
+
+            else
+            {
+                굵게ToolStripMenuItem.Checked = false;
+            }
+
+            if (m_bFill)
+            {
+                채우기ToolStripMenuItem.Checked = true;
+            }
+            else
+            {
+                채우기ToolStripMenuItem.Checked = false;
+            }
+
+        }
+
+        private void 직선ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 직선
+            m_nShape = 0;
+        }
+        private void 원ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 원
+            m_nShape = 1;
+        }
+        private void 네모ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 사각형
+            m_nShape = 2;
+        }
+        private void 채우기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (m_bFill)
+            {
+                m_bFill = false;
+            }
+            else
+            {
+                m_bFill = true;
+            }
+        }
+
+        private void 굵게ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (m_bBold)
+            {
+                m_bBold = false;
+            }
+            else
+            {
+                m_bBold = true;
+            }
+        }
+        private void 저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Shape Myshape = new Shape(startPoint, endPoint, m_bFill, m_bBold, m_nShape);
+
+            FileStream fs = new FileStream(@"c:\Temp\Paint.dat",   // file path
+                                                FileMode.Create,    // file mode
+                                                FileAccess.Write);  // file access
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            bw.Write(ar.Count);
+
+            for (int i = 0; i < ar.Count; i++)
+            {
+                Shape Myshape = new Shape(ar[i].startPoint, ar[i].endPoint, ar[i].m_bFill, ar[i].m_bBold, ar[i].m_nShape);
+                bw.Write(ar[i].startPoint.X);
+                bw.Write(ar[i].startPoint.Y);
+                bw.Write(ar[i].endPoint.X);
+                bw.Write(ar[i].endPoint.Y);
+                bw.Write(ar[i].m_bFill);
+                bw.Write(ar[i].m_bBold);
+                bw.Write(ar[i].m_nShape);
+            }
+            // 초기화 
+            ar.Clear();
+            // 파일 닫기 
+            bw.Close();
+            fs.Close();
+            MessageBox.Show("저장이 완료되었습니다.");
+        }
+
+        private void 불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(@"c:\Temp\Paint.dat",   // file path
+                                                FileMode.Open,    // file mode
+                                                FileAccess.Read);  // file access
+            BinaryReader br = new BinaryReader(fs);
+
+            int count = br.ReadInt32();
+
+            for(int i=0; i< count; i++)
+            {
+                startPoint.X = br.ReadInt32();
+                startPoint.Y = br.ReadInt32();
+                endPoint.X = br.ReadInt32();
+                endPoint.Y = br.ReadInt32();
+                m_bFill = br.ReadBoolean();
+                m_bBold = br.ReadBoolean();
+                m_nShape = br.ReadInt32();
+                Shape Myshape = new Shape(startPoint, endPoint, m_bFill, m_bBold, m_nShape);
+                ar.Add(Myshape);
+                Invalidate();
+            }
+            // 로드가 끝난 후 값 초기화 
+            startPoint.X = 0;
+            startPoint.Y = 0;
+            endPoint.X = 0;
+            endPoint.Y = 0;
+            // 객체에 추가한 후에 m_bFill, m_bBold, m_nShape 는 상태를 유지하고 x,y값은 초기화해서 shape class를 다시 할당 
+            MyShape = new Shape(startPoint, endPoint, m_bFill, m_bBold, m_nShape);
+
+            // 파일 닫기 
+            br.Close();
+            fs.Close();
+            MessageBox.Show("불러오기가 완료되었습니다.");
+        }
+    }
+    class Shape  
+    {
+        public Point startPoint { get; set; }
+        public Point endPoint { get; set; }
+        public bool m_bFill { get; set; }
+        public bool m_bBold { get; set; }
+        public int m_nShape { get; set; }
+
+        public Shape(Point _startPoint, Point _endPoint, bool _m_bFill, bool _m_bBold, int _m_nShape)
+        {
+            startPoint = _startPoint;
+            endPoint = _endPoint;
+            m_bFill = _m_bFill;
+            m_bBold = _m_bBold;
+            m_nShape = _m_nShape;
+        }
+    }
+}
